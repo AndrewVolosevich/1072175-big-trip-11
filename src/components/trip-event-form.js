2c2b5fdfbf9cb40948bf744ef582e7a0345ff0e2
AndrewVolosevich <volosevich1989@gmail.com>
2020-04-16 22:28:40 +0300

    fix

--- src/components/trip-event-form.js
+++ src/components/trip-event-form.js
@@ -20,7 +20,7 @@ export const createTripEventForm = (event) => {
         <div class="event__type-wrapper">
           <label class="event__type  event__type-btn" for="event-type-toggle-1">
             <span class="visually-hidden">Choose event type</span>
-            <img class="event__type-icon" width="17" height="17" src="../img/icons/${type.toLowerCase() ? type.toLowerCase() : `bus`}.png" alt="Event type icon">
+            <img class="event__type-icon" width="17" height="17" src="./img/icons/${type.toLowerCase() ? type.toLowerCase() : `bus`}.png" alt="Event type icon">
           </label>
           <input class="event__type-toggle  visually-hidden"  id="event-type-toggle-1" type="checkbox">
 
