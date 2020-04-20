8f322b21104eefbf93680976bcee5221113c6f38
AndrewVolosevich <volosevich1989@gmail.com>
2020-04-20 23:54:59 +0300

    switch functions to components classes

--- src/components/trip-days.js
+++ src/components/trip-days.js
@@ -1,36 +1,26 @@
-import {createTripEventItem} from './trip-event-item';
-import {MONTH_NAMES} from '../const';
+import {createElement} from "../utils";
 
-const createDatesArr = (tripPoints) => {
-  let startDate = tripPoints[1].startTime.getDate();
-  let lastDate = tripPoints[tripPoints.length - 1].startTime.getDate();
-  const datesArr = [];
-
-  for (let i = startDate; i <= lastDate; i++) {
-    datesArr.push(tripPoints.filter((point) => point.startTime.getDate() === i));
+export default class TripDaysComponent {
+  constructor() {
+    this._element = null;
   }
-  return datesArr;
-};
 
-export const createDayMarkup = (days, dayIndex) => {
-  const dayDate = days[0].startTime;
-  return (
-    `<ul class="trip-days">
-      <li class="trip-days__item  day">
-        <div class="day__info">
-          <span class="day__counter">${dayIndex + 1}</span>
-          <time class="day__date" datetime="2019-03-18">${MONTH_NAMES[dayDate.getMonth()].substr(0, 3)} ${dayDate.getDate()}</time>
-        </div>
-        ${createTripEventItem(days)}
-      </li>
-    </ul>`
-  );
-};
+  getTemplate() {
+    return (
+      `<ul class="trip-days">
+      </ul>`
+    );
+  }
 
-export const createTripDays = (days) => {
-  const dayMarkup = createDatesArr(days).map((it, i) => createDayMarkup(it, i)).join(`\n`);
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
 
-  return `<ul class="trip-days">
-    ${dayMarkup}
-    </ul>`;
-};
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
--- src/components/trip-filters.js
+++ src/components/trip-filters.js
@@ -1,17 +1,40 @@
-const createFilterMarkup = (name, isChecked) => {
-  return (
-    `<div class="trip-filters__filter">
-      <input id="filter-${name.toLowerCase()}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${name.toLowerCase()}" ${isChecked ? `checked` : ``}>
-      <label class="trip-filters__filter-label" for="filter-${name.toLowerCase()}">${name.toLowerCase()}</label>
-    </div>`
-  );
-};
+import {createElement} from "../utils";
 
-export const createTripFilters = (filters) => {
-  const filterMarkup = filters.map((it, i) => createFilterMarkup(it, i === 0)).join(`\n`);
+export default class TripFiltersComponent {
+  constructor(filters) {
+    this._filters = filters;
+    this._element = null;
+  }
 
-  return `<form class="trip-filters" action="#" method="get">
-    ${filterMarkup}
-    <button class="visually-hidden" type="submit">Accept filter</button>
-    </form>`;
-};
+  getMarkup(name, isChecked) {
+    const lowerName = name.toLowerCase();
+    return (
+      `<div class="trip-filters__filter">
+        <input id="filter-${lowerName}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${lowerName}" ${isChecked ? `checked` : ``}>
+        <label class="trip-filters__filter-label" for="filter-${lowerName}">${lowerName}</label>
+      </div>`
+    );
+  }
+
+  getTemplate(filters) {
+    const filterMarkup = filters.map((it, i) => this.getMarkup(it, i === 0)).join(`\n`);
+
+    return (
+      `<form class="trip-filters" action="#" method="get">
+        ${filterMarkup}
+        <button class="visually-hidden" type="submit">Accept filter</button>
+      </form>`
+    );
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate(this._filters));
+    }
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
