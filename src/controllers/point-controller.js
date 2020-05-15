594ac07ad083f664d574971e6c509023818c3d4f
AndrewVolosevich <volosevich1989@gmail.com>
2020-05-15 14:02:41 +0300

    start commit module6-task1

---
+++ src/controllers/point-controller.js
@@ -0,0 +1,14 @@
+import TripEventItemComponent from "../components/trip-event-item";
+import TripEventFormComponent from "../components/trip-event-form";
+import {render, replace, RenderPosition} from "../utils/render";
+
+
+export default class PointController {
+  constructor(container) {
+
+  }
+
+  render(point) {
+
+  }
+}
